59fac8c9aebf568f5a9fbb5b7afd8b6577e45861
fanwei <fanerwei222@163.com>
2017-12-28 16:36:06 +0800

    更改了springboot版本.不然会运行部起来. jar包有冲突.

---
+++ src/main/java/com/ulyne/application/ServiceRibbonApplication.java
@@ -0,0 +1,29 @@
+package com.ulyne.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *在工程的启动类中,通过@EnableDiscoveryClient向服务中心注册；并且向程序的ioc注入一个bean: restTemplate;并通过@LoadBalanced注解表明这个restRemplate开启负载均衡的功能。
+ * Created by fanwei_last on 2017/12/28.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.ulyne")//扫描该文件夹下面的所有控制器什么的
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
